863c5de281e6183469fd409c698657af3f7834be
ojasdkg <ojas@dkglabs.com>
2025-06-02 10:02:24 +0000

    change

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Starting Spring Boot Application...");
+		System.out.println("Starting abcd Spring Boot Application...");
 		SpringApplication.run(Main.class, args);
 	}
 
